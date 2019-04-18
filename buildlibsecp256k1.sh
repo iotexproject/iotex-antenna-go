4ded9eb413705bdb68b6e36352c6d3fe622a771d
lzxm160 <lzxm160@sina.com>
2019-04-18 09:04:25 +0800

    add sudo

--- buildlibsecp256k1.sh
+++ buildlibsecp256k1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt-get update
-apt-get install openssl -y
-apt-get install libssl-dev -y
-apt-get install autoconf -y
-apt-get install default-jre -y
-apt-get install build-essential automake libtool pkg-config libffi-dev python-dev python-pip -y
+sudo apt-get install openssl -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install autoconf -y
+sudo apt-get install default-jre -y
+sudo apt-get install build-essential automake libtool pkg-config libffi-dev python-dev python-pip -y
 cd vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/libsecp256k1
 ./autogen.sh
 ./configure --disable-shared --with-pic --with-bignum=no --enable-module-recovery --disable-jni
 make
-make install
+sudo make install
