6f7c5b63fbd718f0543e9619d16e9a657a93d079
JonSnow47 <745539141@qq.com>
2020-07-11 22:18:50 +0800

    fix TestStake first commit
    1. try to use gomock to test client, but not sure I'm right. I need help.
    2. I have no idea about store interfaces_mock.go, when I gen the file as iotex-antena-go/test/mock/interfaces_mock.go, cuz import cycle.

--- iotex/caller_staking_test.go
+++ iotex/caller_staking_test.go
@@ -4,61 +4,184 @@ import (
 	"context"
 	"math/big"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/require"
-
+	"github.com/golang/mock/gomock"
 	"github.com/iotexproject/go-pkgs/hash"
 	"github.com/iotexproject/iotex-address/address"
-	"github.com/iotexproject/iotex-proto/golang/iotexapi"
-	"github.com/iotexproject/iotex-proto/golang/iotextypes"
+	"github.com/stretchr/testify/require"
 
-	"github.com/iotexproject/iotex-antenna-go/v2/account"
 	"github.com/iotexproject/iotex-antenna-go/v2/utils/unit"
 )
 
-func TestStake(t *testing.T) {
+var (
+	testActionHash1, _ = hash.HexStringToHash256("16fbdac39a19f433b32e457eba9c64c48d9fafcdffaddb11a0b7d264c0cf1418")
+	// CandidateRegister tests.
+	candidateRegisterTests = []struct {
+		// input
+		name      string
+		operator  string
+		reward    string
+		amount    *big.Int
+		duration  uint32
+		autoStake bool
+		gasPrice  *big.Int
+		gasLimit  uint64
+		payload   []byte
+		// expect
+		actionHash hash.Hash256
+	}{
+		{
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			big.NewInt(int64(unit.Iotx)),
+			10000,
+			false,
+			big.NewInt(int64(10 * unit.Qev)),
+			1000000,
+			[]byte("TestCandidateRegister"),
+			testActionHash1,
+		},
+	}
+	// Stake tests.
+	stakeTests = []struct {
+		candidateName string
+		amount        *big.Int
+		duration      uint32
+		autoStake     bool
+		gasPrice      *big.Int
+		gasLimit      uint64
+		payload       []byte
+		// expect
+		actionHash hash.Hash256
+	}{
+		{
+			"io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he",
+			big.NewInt(int64(unit.Iotx)),
+			10000,
+			false,
+			big.NewInt(int64(10 * unit.Qev)),
+			1000000,
+			[]byte("TestStaking"),
+			testActionHash1,
+		},
+	}
+	// Unstake tests.
+	unstakeTests = []struct {
+		bucketIndex uint64
+		autoStake   bool
+		gasPrice    *big.Int
+		gasLimit    uint64
+		payload     []byte
+		// expect
+		actionHash hash.Hash256
+	}{
+		{
+			10000,
+			false,
+			big.NewInt(int64(10 * unit.Qev)),
+			1000000,
+			[]byte("TestStaking"),
+			testActionHash1,
+		},
+	}
+)
+
+func TestCandidateCaller_Register(t *testing.T) {
 	require := require.New(t)
-	conn, err := NewDefaultGRPCConn(_testnet)
-	require.NoError(err)
-	defer conn.Close()
 
-	acc, err := account.HexStringToAccount(_accountPrivateKey)
-	require.NoError(err)
-	c := NewAuthedClient(iotexapi.NewAPIServiceClient(conn), acc)
-	name, _ := address.FromString("io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he")
-	operator, _ := address.FromString("io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he")
-	reward, _ := address.FromString("io10a298zmzvrt4guq79a9f4x7qedj59y7ery84he")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	// CandidateRegister
-	ret, err := c.Candidate().Register(name, operator, reward, big.NewInt(1), 10, false).SetGasPrice(big.NewInt(int64(unit.Qev))).SetGasLimit(1000000).SetPayload([]byte("payload")).Call(context.Background())
-	require.Error(err)
-	require.Equal(hash.ZeroHash256, ret)
+	testTimes := len(candidateRegisterTests)
 
-	// need to fix when testnet ready
-	//time.Sleep(time.Second * 20)
-	//receipt, err := c.GetReceipt(ret).Call(context.Background())
-	//require.NoError(err)
-	//require.Equal(iotextypes.ReceiptStatus_Success, receipt.ReceiptInfo.Receipt.Status)
+	stakingAPICaller := NewMockStakingAPICaller(ctrl)
+	stakingAPICaller.EXPECT().SetGasPrice(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetGasLimit(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetPayload(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	candidateCaller := NewMockCandidateCaller(ctrl)
+	candidateCaller.EXPECT().Register(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	client := NewMockAuthedClient(ctrl)
+	client.EXPECT().Candidate().Return(candidateCaller).Times(testTimes)
 
-	// StakeCreate
-	ret, err = c.Staking().Create("io19d0p3ah4g8ww9d7kcxfq87yxe7fnr8rpth5shj", big.NewInt(100), uint32(10000), true).SetGasPrice(big.NewInt(int64(unit.Qev))).SetGasLimit(1000000).SetPayload([]byte("payload")).Call(context.Background())
-	require.Error(err)
-	require.Equal(hash.ZeroHash256, ret)
+	for _, test := range candidateRegisterTests {
+		stakingAPICaller.EXPECT().Call(gomock.Any()).Return(test.actionHash, nil).Times(1)
 
-	// need to fix when testnet ready
-	//time.Sleep(time.Second * 10)
-	//receipt, err = c.GetReceipt(hash).Call(context.Background())
-	//require.NoError(err)
-	//require.Equal(iotextypes.ReceiptStatus_Success, receipt.ReceiptInfo.Receipt.Status)
+		nameAddr, err := address.FromString(test.name)
+		require.NoError(err)
+		operatorAddr, err := address.FromString(test.operator)
+		require.NoError(err)
+		rewardAddr, err := address.FromString(test.reward)
+		require.NoError(err)
+		ret, err := client.Candidate().
+			Register(nameAddr, operatorAddr, rewardAddr, test.amount, test.duration, test.autoStake).
+			SetGasPrice(test.gasPrice).
+			SetGasLimit(test.gasLimit).
+			SetPayload(test.payload).
+			Call(context.Background())
+		require.NoError(err)
+		require.Equal(test.actionHash, ret)
+	}
+}
 
-	// StakeUnstake
-	ret, err = c.Staking().Unstake(1).SetGasPrice(big.NewInt(int64(unit.Qev))).SetGasLimit(1000000).SetPayload([]byte("payload")).Call(context.Background())
-	require.NoError(err)
-	require.NotEqual(hash.ZeroHash256, ret)
-	// need to fix when testnet ready
-	time.Sleep(time.Second * 10)
-	receipt, err := c.GetReceipt(ret).Call(context.Background())
-	require.NoError(err)
-	require.NotEqual(iotextypes.ReceiptStatus_Success, receipt.ReceiptInfo.Receipt.Status)
+func TestStakingCaller_Create(t *testing.T) {
+	require := require.New(t)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testTimes := len(stakeTests)
+
+	stakingAPICaller := NewMockStakingAPICaller(ctrl)
+	stakingAPICaller.EXPECT().SetGasPrice(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetGasLimit(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetPayload(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingCaller := NewMockStakingCaller(ctrl)
+	stakingCaller.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	clt := NewMockAuthedClient(ctrl)
+	clt.EXPECT().Staking().Return(stakingCaller).Times(testTimes)
+
+	for _, test := range stakeTests {
+		stakingAPICaller.EXPECT().Call(gomock.Any()).Return(test.actionHash, nil).Times(1)
+
+		ret, err := clt.Staking().
+			Create(test.candidateName, test.amount, test.duration, test.autoStake).
+			SetGasPrice(test.gasPrice).
+			SetGasLimit(test.gasLimit).
+			SetPayload(test.payload).
+			Call(context.Background())
+		require.NoError(err)
+		require.Equal(test.actionHash, ret)
+	}
+}
+
+func TestStakingCaller_Unstake(t *testing.T) {
+	require := require.New(t)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testTimes := len(unstakeTests)
+
+	stakingAPICaller := NewMockStakingAPICaller(ctrl)
+	stakingAPICaller.EXPECT().SetGasPrice(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetGasLimit(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingAPICaller.EXPECT().SetPayload(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	stakingCaller := NewMockStakingCaller(ctrl)
+	stakingCaller.EXPECT().Unstake(gomock.Any()).Return(stakingAPICaller).Times(testTimes)
+	clt := NewMockAuthedClient(ctrl)
+	clt.EXPECT().Staking().Return(stakingCaller).Times(testTimes)
+
+	for _, test := range unstakeTests {
+		stakingAPICaller.EXPECT().Call(gomock.Any()).Return(test.actionHash, nil).Times(1)
+
+		ret, err := clt.Staking().
+			Unstake(test.bucketIndex).
+			SetGasPrice(test.gasPrice).
+			SetGasLimit(test.gasLimit).
+			SetPayload(test.payload).
+			Call(context.Background())
+		require.NoError(err)
+		require.Equal(test.actionHash, ret)
+	}
 }
